534c2322840ef12b15dffc11c2e226e68a86b1ca
dkmould <amiyaranjansethi0551@gmail.com>
2020-05-10 02:06:43 +0530

    amiya

---
+++ create.js
@@ -0,0 +1,14 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	
+}
+
+function draw() {
+	const ard = map(mouseX,0,width,0,255);
+	const ard1 =map(mouseY,0,height,0,255);
+	translate(mouseX,mouseY);
+	const amiyafury =map (mouseY,0,height,0,TWO_PI);
+	rotate(amiyafury);
+	stroke(ard,ard1,255);
+	line(-50,0,50,0);
+}
